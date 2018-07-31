dc84910c92e11dda73b770069e65a35405b7981c
Bishoy-Tawfik <btprogramer@gmail.com>
2018-07-30 21:45:52 -0700

    Adding maniest.json file and applying enhancements on the progressive web app

--- sw.js
+++ sw.js
@@ -1,48 +1,47 @@
-
 const cacheName = `v1`;
 
 self.addEventListener('install', e => {
     const timeStamp = Date.now();
     e.waitUntil(
-      caches.open(cacheName).then(cache => {
-        return cache.addAll([
-          `/`,
-          `/index.html`,
-          `/restaurant.html`,
-          `/js/dbhelper.js`,
-          `/js/main.js`,
-          `/js/restaurant_info.js`,
-          `/css/styles.css`,
-          `/data/restaurants.json`,
-          `/img/1.jpg`,
-          `/img/2.jpg`,
-          `/img/3.jpg`,
-          `/img/4.jpg`,
-          `/img/5.jpg`,
-          `/img/6.jpg`,
-          `/img/7.jpg`,
-          `/img/8.jpg`,
-          `/img/9.jpg`,
-          `/img/10.jpg`
-        ])
-            .then(() => self.skipWaiting());
-      })
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                    `/`,
+                    `/index.html`,
+                    `/manifest.json`,
+                    `/restaurant.html`,
+                    `/js/dbhelper.js`,
+                    `/js/main.js`,
+                    `/js/restaurant_info.js`,
+                    `/css/styles.css`,
+                    `/data/restaurants.json`,
+                    `/img/1.jpg`,
+                    `/img/2.jpg`,
+                    `/img/3.jpg`,
+                    `/img/4.jpg`,
+                    `/img/5.jpg`,
+                    `/img/6.jpg`,
+                    `/img/7.jpg`,
+                    `/img/8.jpg`,
+                    `/img/9.jpg`,
+                    `/img/10.jpg`
+                ])
+                .then(() => self.skipWaiting());
+        })
     );
-  });
+});
 
 
 
 self.addEventListener('activate', event => {
     event.waitUntil(self.clients.claim());
-  });
-  
-  self.addEventListener('fetch', event => {
+});
+
+self.addEventListener('fetch', event => {
     event.respondWith(
-      caches.open(cacheName)
-        .then(cache => cache.match(event.request, {ignoreSearch: true}))
+        caches.open(cacheName)
+        .then(cache => cache.match(event.request, { ignoreSearch: true }))
         .then(response => {
-        return response || fetch(event.request);
-      })
+            return response || fetch(event.request);
+        })
     );
-  });
-  
+});

624292639e9d47b1d35fc9237d6d5d427220b765
Bishoy Tawfik <btawfik@altamed.local>
2018-08-28 19:13:09 -0700

    Creating IndexedDB

--- sw.js
+++ sw.js
@@ -1,6 +1,27 @@
+if (typeof idb === "undefined") {
+    self.importScripts('./node_modules/idb/lib/idb.js');
+}
 const cacheName = `v1`;
 
 self.addEventListener('install', e => {
+//     var dbPromise = idb.open("myDB",1);
+//        dbPromise.createObjectStore("firstOS");
+//         var transaction = dbPromise.transaction(["people"],"readwrite");
+//         var store = transaction.objectStore("people");
+//         //Define a person
+// var person = {
+//     name:name,
+//     email:email,
+//     created:new Date()
+// }
+ 
+// //Perform the add
+// var request = store.add(person,1);
+
+
+
+
+
     const timeStamp = Date.now();
     e.waitUntil(
         caches.open(cacheName).then(cache => {
@@ -9,11 +30,9 @@ self.addEventListener('install', e => {
                     `/index.html`,
                     `/manifest.json`,
                     `/restaurant.html`,
-                    `/js/dbhelper.js`,
                     `/js/main.js`,
                     `/js/restaurant_info.js`,
                     `/css/styles.css`,
-                    `/data/restaurants.json`,
                     `/img/1.jpg`,
                     `/img/2.jpg`,
                     `/img/3.jpg`,
@@ -37,6 +56,11 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+    var dbPromise = idb.open('restaurantsDB', 1, function(upgradeDb) {
+        if (!upgradeDb.objectStoreNames.contains('restaurants')) {
+          var restaurantsOS = upgradeDb.createObjectStore('restaurants', {keyPath: 'id'});
+        }
+      });
     event.respondWith(
         caches.open(cacheName)
         .then(cache => cache.match(event.request, { ignoreSearch: true }))

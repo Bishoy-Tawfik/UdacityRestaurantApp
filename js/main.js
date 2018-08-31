f9a7130d354e2f418656d8a2b253ca4d9036bd49
Bishoy-Tawfik <btprogramer@gmail.com>
2018-08-31 00:49:12 -0700

    Fixing accessibility issues

--- js/main.js
+++ js/main.js
@@ -156,7 +156,7 @@ createRestaurantHTML = (restaurant) => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
-    image.alt = restaurant.imgalt;
+    image.alt = 'Image of restaurant ' + restaurant.name;
     li.append(image);
 
     const name = document.createElement('h1');
